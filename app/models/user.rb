45e1d9837873dea839107d9940b475094bb5b971
ephesh2 <e.ph.esh2@gmail.com>
2023-04-17 20:28:51 +0900

    Rubocopの自動修正を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-        
 end
