42403179e58a4b3ca9105db7ebaf73b07ec5f387
ephesh2 <e.ph.esh2@gmail.com>
2023-04-18 19:27:10 +0900

    バリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to [:category, :item_status, :prefecture, :delivery_charge, :ship_date]
+  belongs_to :categor
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :delivery_charge
+  belongs_to :ship_date
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :detail, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :ship_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price, presence: true
 end
