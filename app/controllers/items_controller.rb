55fdb2982fd9060b072f7293eb5f7f9ea3635828
ephesh2 <e.ph.esh2@gmail.com>
2023-04-19 20:14:47 +0900

    ログアウト時はログインページへリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: :new
+
   def index; end
 
   def new
@@ -17,5 +19,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:title, :detail, :category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless  user_signed_in?
+      redirect_to new_user_session_path      
+    end
   end
 end
