49b3801dbb879eb02cb52f099b76ecc2afeae61c
ephesh2 <e.ph.esh2@gmail.com>
2023-04-22 17:21:57 +0900

    ログインページへの自動遷移をauthenticate_user!に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in, only: %i[new edit]
+  before_action :authenticate_user!, only: %i[new edit]
   before_action :find_item, only: %i[show edit update]
 
   def index
@@ -46,11 +46,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:title, :detail, :category_id, :item_status_id, :delivery_charge_id, :prefecture_id,
                                  :ship_date_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_sign_in
-    return if user_signed_in?
-
-    redirect_to new_user_session_path
   end
 end
